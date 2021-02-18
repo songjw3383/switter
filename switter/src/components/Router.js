5dcab9ffb21b6dc788cca2a2d55d488b574e89ff
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-02-19 01:57:02 +0900

    0219

--- switter/src/components/Router.js
+++ switter/src/components/Router.js
@@ -1,25 +1,34 @@
 import { React, useState} from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom"
+import {HashRouter as Router, Route, Switch, Redirect} from "react-router-dom"
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+const AppRouter = ({ isLoggedIn }) => {
     return (
-        <Router>
-            <Switch>
-                {isLoggedIn ? 
-                <>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                </> : 
-                <Route exact path="/">
-                     <Auth />
-                </Route>}
-            </Switch>
-        </Router>
-    )
-}
-
+      <Router>
+        {isLoggedIn && <Navigation />}
+        <Switch>
+          {isLoggedIn ? (
+            <>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+            </>
+          ) : (
+            <>
+              <Route exact path="/">
+                <Auth />
+              </Route>
+            </>
+          )}
+        </Switch>
+      </Router>
+    );
+          };
 export default AppRouter;
