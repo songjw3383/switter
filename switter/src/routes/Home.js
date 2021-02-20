984065cf90f674bf329ef739ecc9e6d1310dfd2d
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-02-20 21:46:25 +0900

    0220

--- switter/src/routes/Home.js
+++ switter/src/routes/Home.js
@@ -1,4 +1,44 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
 
-const Home = () => <span>Home</span>
+const Home = ({userObj}) => {
+    const [sweet, setSweet] = useState("");
+    const [sweets, setSweets] = useState([]);
+    useEffect(() => {  
+        dbService.collection("sweets").onSnapshot(snapshot => {
+            const sweetArray = snapshot.docs.map(doc => ({id:doc.id, ...doc.data()
+            }));
+            setSweets(sweetArray);
+        });
+    }, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.collection("sweets").add({
+            text: sweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid, 
+        })
+        setSweet("");
+    };
+    const onChange = (event) => {
+        const { target:{value}
+        } = event;
+        setSweet(value);
+    }
+    return ( 
+    <div>
+        <form onSubmit={onSubmit}>
+            <input value={sweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="submit" value="Sweet" />
+        </form>
+        <div>
+            {sweets.map(sweet => (
+            <div key={sweet.id}>
+                <h4>{sweet.text}</h4>
+            </div>
+            ))}
+        </div>
+    </div>
+    )
+}
 export default Home;
